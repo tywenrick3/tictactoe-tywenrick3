2d37589cca0a8b83ba4822b77e96573922f80925
tywenrick3 <tew287@nyu.edu>
2021-04-13 00:20:22 -0400

    Initial commit

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,4 +1,4 @@
-# Author: <Your name>
+# Author: <Ty Wenrick>
 # Assignment #5 - Tic-tac-toe
 # Date due: 2021-04-22
 # I pledge that I have completed this assignment without
@@ -14,6 +14,7 @@ NUM_COLS = 3
 NUM_POSITIONS = 9
 ROW_POS = 0
 COL_POS = 1
+
 
 def display_board(board):
     """Displays the board's current state as a 3x3 grid"""
@@ -37,6 +38,77 @@ def display_board(board):
 ####### DO NOT EDIT CODE ABOVE ########
 
 
+def reset_board(board):
+    """Resets the board dict to its original state with each
+    position being empty (i.e. the (row, column) key has a
+    space character (' ') value).
+
+    :param board: a dict of (row, column) tuple keys and string values
+    :return: None
+
+    >>> board = {(0, 0): 'X', (0, 1): ' ', (0, 2): ' ', (1, 0): 'O', (1, 1): ' ', (1, 2): 'O', (2, 0): 'X', (2, 1): ' ', (2, 2): ' '}
+    >>> reset_board(board)
+    >>> board
+    {(0, 0): ' ', (0, 1): ' ', (0, 2): ' ', (1, 0): ' ', (1, 1): ' ', (1, 2): ' ', (2, 0): ' ', (2, 1): ' ', (2, 2): ' '}
+    """
+    for key in board:
+        board[key] = ' '
+
+
+def get_current_player(round):
+    """Returns the mark of the player whose turn it is in the current
+    round of the game.
+
+    :param round: integer value representing the round
+    :return: 'X' or 'O' depending on round
+
+    >>> round =  3
+    >>> get_current_player(round)
+    'O'
+    >>> round = 8
+    >>> get_current_player(round)
+    'X'
+    """
+    if round % 2 == 0:
+        return 'X'
+    else:
+        return 'O'
+
+
+def get_position_choice(board, player_mark):
+    """Prompts the user for a valid (row, col) board position. Prompts
+    for row and column are repeated until valid position provided. The
+    valid (row, col) position chosen is returned.
+
+    :param board: a dict of (row, col) tuple keys and string values
+    :param player_mark: 'X' or 'O' depending on round
+    :return: (row, col) tuple of integers representing valid position choice
+    """
+    outputs = ()
+    print(player_mark + ',')
+    restart = 0
+    while restart == 0:
+        choices = (0, 1, 2)
+        while True:
+            row_choice = input("Choose your row: ")
+            if row_choice.isdigit():
+                if int(row_choice) in choices:
+                    break
+
+        while True:
+            col_choice = input("Choose your column: ")
+            if col_choice.isdigit():
+                if int(col_choice) in choices:
+                    outputs = (int(row_choice), int(col_choice))
+                    if board[outputs] == ' ':
+                        restart += 1
+                    else:
+                        break
+        break
+
+    return outputs
+
+
 def main():
 
     ########## DO NOT EDIT DICTIONARY INITIALIZATION BELOW #########
@@ -47,10 +119,15 @@ def main():
         (2,0): ' ', (2,1): ' ', (2,2): ' '
     }
 
+
+
     ########## DO NOT EDIT DICTIONARY INITIALIZATION ABOVE #########
     
     # call play_tic_tac_toe() with board as argument and remove pass below
-    pass
+    board = {(0, 0): 'X', (0, 1): ' ', (0, 2): ' ', (1, 0): 'O', (1, 1): ' ', (1, 2): 'O', (2, 0): 'X', (2, 1): ' ', (2, 2): ' '}
+    player_mark = 'O'
+    print(get_position_choice(board, player_mark))
+
 
 if __name__ == '__main__':
     main()
