713fe719d9ef1571804fcd6e572f15b32ab661d0
Ty Wenrick <tew287@nyu.edu>
2021-04-22 01:19:36 -0400

    is_game_complete() updated

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -213,7 +213,7 @@ def is_game_complete(board):
     # checks 8 possible winning gameboards
 
     # diagonal 1
-    if (not board[(0, 0)] == " ") and (not board[(1, 1)] == " ") and (not board[(2, 2)] == " "):
+    if not board[(0, 0)] == " " and not board[(1, 1)] == " " and not board[(2, 2)] == " ":
         pos1_value = board[(0, 0)]
         pos2_value = board[(1, 1)]
         pos3_value = board[(2, 2)]
@@ -221,7 +221,7 @@ def is_game_complete(board):
             return True
 
     # diagonal 2
-    elif (not board[(0, 2)] == " ") and (not board[(1, 1)] == " ") and (not board[(2, 0)] == " "):
+    elif not board[(0, 2)] == " " and not board[(1, 1)] == " " and not board[(2, 0)] == " ":
         pos1_value = board[(0, 2)]
         pos2_value = board[(1, 1)]
         pos3_value = board[(2, 0)]
@@ -229,7 +229,7 @@ def is_game_complete(board):
             return True
 
     # vertical 1
-    elif (not board[(0, 0)] == " ") and (not board[(1, 0)] == " ") and (not board[(2, 0)] == " "):
+    elif not board[(0, 0)] == " " and not board[(1, 0)] == " " and not board[(2, 0)] == " ":
         pos1_value = board[(0, 0)]
         pos2_value = board[(1, 0)]
         pos3_value = board[(2, 0)]
@@ -237,7 +237,7 @@ def is_game_complete(board):
             return True
 
     # vertical 2
-    elif (not board[(0, 1)] == " ") and (not board[(1, 1)] == " ") and (not board[(2, 1)] == " "):
+    elif not board[(0, 1)] == " " and not board[(1, 1)] == " " and not board[(2, 1)] == " ":
         pos1_value = board[(0, 1)]
         pos2_value = board[(1, 1)]
         pos3_value = board[(2, 1)]
@@ -245,7 +245,7 @@ def is_game_complete(board):
             return True
 
     # vertical 3
-    elif (not board[(0, 2)] == " ") and (not board[(1, 2)] == " ") and (not board[(2, 2)] == " "):
+    elif not board[(0, 2)] == " " and not board[(1, 2)] == " " and not board[(2, 2)] == " ":
         pos1_value = board[(0, 2)]
         pos2_value = board[(1, 2)]
         pos3_value = board[(2, 2)]
